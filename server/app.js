2632306610194186ed969e3712c7de160f23992b
Bagas Nurseno Achmad <bagasnurseno@gmail.com>
2025-11-03 00:58:10 +0700

    feat: add express middleware

--- server/app.js
+++ server/app.js
@@ -1,5 +1,14 @@
-const express = require('express')
-const app = express()
+// Import dan definiskan aplikasi utama dari express.js
+const express = require('express');
+const app = express();
+
+// Import middleware dan masukkan ke aplikasi express.js
+const cors = require('cors');
+const morgan = require('morgan');
+
+app.use(cors());
+app.use(morgan('dev'))
+
 const port = 3000
 
 // Untuk static file yang ada pada folder public (termasuk front-end)
